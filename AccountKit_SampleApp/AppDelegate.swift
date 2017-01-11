b5396b067d87094222554b8b54037e06ed970876
Jarrod Parkes <jarrod@udacity.com>
2017-01-11 09:59:45 -0600

    add comments

--- AccountKit_SampleApp/AppDelegate.swift
+++ AccountKit_SampleApp/AppDelegate.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // MARK: Properties
+    
     var window: UIWindow?
+    
+    // MARK: UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 }
-
